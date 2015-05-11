2e50efd52ea89b841eb22acc87655d2081b3dc4b
Betty L. Hutchings <blhutch4@gmail.com>
2015-05-11 17:16:29 -0400

    It works

--- HW4/tic_two_toe.rb
+++ HW4/tic_two_toe.rb
@@ -1,15 +1,32 @@
 require 'pry'
 
 def print_board(board)
-  puts '   |   |   ' 
-  puts ' #{board[0]} | #{board[1]} | #{board[2]} '
-  puts '___|___|___'
-  puts '   |   |   ' 
-  puts ' #{board[3]} | #{board[4]} | #{board[5]} '
-  puts '___|___|___'
-  puts '   |   |   '
-  puts ' #{board[6]} | #{board[7]} | #{board[8]} '
-  puts '   |   |   '
+  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "___|___|___"
+  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "___|___|___"
+  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts "   |   |   "
+end
+
+WINS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
+       [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+def win?(board)
+  WINS.any? do |x, y, z|
+    board[x] == board [y] && board[y] == board[z]
+  end
+end
+
+def draw?(board)
+  board.all? { |x| x.is_a? String }
+end
+
+def game_over?(board)
+  win?(board) || draw?(board)
 end
 
 def take_turn(turn_count, board)
@@ -42,22 +59,32 @@ def take_turn(turn_count, board)
   end  
 end
 
-def begin_game
+def begin_game(board)
   board = (1..9).to_a
   puts
   puts 'Welcome to Tic-Tac-Toe.'
   puts
   print_board(board)
   puts
-  return board
-end  
+  board
+end 
 
-board = begin_game
-[0, 1, 2, 3].each_with_index do |n, i| 
-  board = take_turn(i, board)
-  puts
-  print_board(board)
-  puts
+board = begin_game(board)
+turn_count = 0
+  until win?(board) || draw?(board)
+    take_turn(turn_count, board)
+    turn_count +=1
+    puts
+    print_board(board)
+    puts
+  end
+
+if win?(board) == true
+  puts 'We have a winner!'
+end
+
+if draw?(board) == true
+  puts 'We have a draw!'
 end
 
 binding.pry
